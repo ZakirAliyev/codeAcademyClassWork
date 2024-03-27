9ef71448d70ba9a98bad56dffbb332bfcfde7238
ZakirAliyev <allahverdiyeva.qizilgul2003gmail.com>
2024-03-27 07:57:19 -0800

    Lesson 11 - 27.03

---
+++ Lesson11/Problem1/assets/js/script.js
@@ -0,0 +1,38 @@
+// console.log("Hello, JavaScript!");
+
+// window.alert("Hello, JavaScript!");
+
+// Problem 1
+// let value = window.prompt("Enter second: ");
+// let minute = parseInt(value / 60);
+// let second = value % 60;
+// console.log(`${minute} minute and ${second} second.`);
+
+// Problem 2
+// let value = window.prompt("Enter temperature: ");
+// let temperature = (5 / 9) * (value - 32);
+// console.log(`Temperature is ${temperature} celcius.`);
+
+// Problem 3
+// let sum = 0;
+// for (let i = 0; i < 5; i++) {
+//   sum += +window.prompt(`Enter ${i + 1}.point : `);
+// }
+// console.log(`Average point : ${sum / 5}`);
+
+// Problem 4
+// let value = +window.prompt("Enter your age: ");
+// let age = 2024 - value;
+// console.log(`Your birth year is ${age}.`);
+
+// Problem 5
+// let number = +window.prompt("Enter number: ");
+// let percent = +window.prompt("Enter percent: ");
+// let result = (number * percent) / 100;
+// console.log(`${percent} percent of ${number}: ${result}`);
+
+// Problem 6
+// let cathetus1 = +window.prompt("Enter 1.cathetus: ");
+// let cathetus2 = +window.prompt("Enter 2.cathetus: ");
+// let result = (cathetus1 * cathetus2) / 2;
+// console.log(`Area of triangle: ${result}`);
